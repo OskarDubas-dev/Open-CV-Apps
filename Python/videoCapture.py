b3f602dfce74a4174ef60342ce4088d4ea6ed98a
OskarDubas-dev <oohdee.dev@gmail.com>
2022-09-13 12:55:29 +0100

    Camera capture

--- Python/videoCapture.py
+++ Python/videoCapture.py
@@ -1,2 +1,20 @@
-import cv2
+import cv2, time
+
+video = cv2.VideoCapture(0)
+
+
+while True:
+    key=cv2.waitKey(1)
+    check, frame = video.read()
+    cv2.imshow("video", frame)
+    if key==ord('q'):
+        break
+
+
+    
+    
+    
+video.release()
+
+cv2.destroyAllWindows()
 
