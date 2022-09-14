0e0da4e2b5c3b1176f2c97ee44dba467adf3a97c
OskarDubas-dev <oohdee.dev@gmail.com>
2022-09-14 13:45:33 +0100

    1st frame added

--- Python/videoCapture.py
+++ Python/videoCapture.py
@@ -1,11 +1,18 @@
 import cv2, time
 
+
+first_frame = None
 video = cv2.VideoCapture(0)
 
 
 while True:
     key=cv2.waitKey(1)
     check, frame = video.read()
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    if first_frame == None:
+        first_frame = gray
+    
     cv2.imshow("video", frame)
     if key==ord('q'):
         break
