d02decc7f10a21e7d39c44bce5f9b841e2680d40
OskarDubas-dev <oohdee.dev@gmail.com>
2022-09-14 19:26:20 +0100

    GaussianBlur and absdiff in video capture added

--- Python/videoCapture.py
+++ Python/videoCapture.py
@@ -9,11 +9,16 @@ while True:
     key=cv2.waitKey(1)
     check, frame = video.read()
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-
-    if first_frame == None:
+    gray = cv2.GaussianBlur(gray, (21,21),0)
+    if first_frame is None:
         first_frame = gray
+        continue
     
-    cv2.imshow("video", frame)
+    delta_frame = cv2.absdiff(gray, first_frame)
+
+    cv2.imshow("video", gray)
+    cv2.imshow("delta", delta_frame)
+    cv2.imshow("normal", frame)
     if key==ord('q'):
         break
 
