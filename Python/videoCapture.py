1e3e81659656bf61587748b638340f4b5e5742d2
OskarDubas-dev <oohdee.dev@gmail.com>
2022-10-20 13:23:54 +0100

    trackbar

--- Python/videoCapture.py
+++ Python/videoCapture.py
@@ -1,9 +1,24 @@
 import cv2, time
 
+from cv2 import WINDOW_AUTOSIZE
+from cv2 import WINDOW_NORMAL
+from cv2 import resizeWindow
+
 
 first_frame = None
 video = cv2.VideoCapture(0)
+thr = 30
 
+trackbarWindow = "threshWindow"
+cv2.namedWindow(trackbarWindow, WINDOW_AUTOSIZE)
+
+def on_trackbar(val):
+    pass
+
+#trackbars = cv2.namedWindow("Trackbars", (640,200))
+#trackbars = cv2.createTrackbar("threshold", thr, 0)
+#resizeWindow(trackbarWindow, 640,480)
+cv2.createTrackbar("Threshold", trackbarWindow, 30, 100, on_trackbar)
 
 while True:
     key=cv2.waitKey(1)
@@ -14,11 +29,15 @@ while True:
         first_frame = gray
         continue
     
+    thr = int(cv2.getTrackbarPos("Threshold",trackbarWindow))
+
     delta_frame = cv2.absdiff(gray, first_frame)
+    delta_threshold = cv2.threshold(delta_frame,thr,255,cv2.THRESH_BINARY)[1]
 
     cv2.imshow("video", gray)
     cv2.imshow("delta", delta_frame)
     cv2.imshow("normal", frame)
+    cv2.imshow("Thresh", delta_threshold)
     if key==ord('q'):
         break
 
