427f62a798a82c7179434b33bb86b75818d8fe5a
OskarDubas-dev <oohdee.dev@gmail.com>
2022-10-25 12:01:47 +0100

    Program records the time when movement is detected

--- Python/videoCapture.py
+++ Python/videoCapture.py
@@ -1,17 +1,26 @@
-import cv2, time
+import cv2, time, pandas
 
 from cv2 import WINDOW_AUTOSIZE
 from cv2 import WINDOW_NORMAL
 from cv2 import resizeWindow
 
+from datetime import datetime
+
+
+
 
 first_frame = None
 video = cv2.VideoCapture(0)
 thr = 30
-status = 0
+
+
+
+times = []
+df = pandas.DataFrame(columns = ["Start", "End"])
+
 
 trackbarWindow = "threshWindow"
-cv2.namedWindow(trackbarWindow, WINDOW_AUTOSIZE)
+#cv2.namedWindow(trackbarWindow, WINDOW_AUTOSIZE)
 
 def on_trackbar(val):
     pass
@@ -19,9 +28,15 @@ def on_trackbar(val):
 #trackbars = cv2.namedWindow("Trackbars", (640,200))
 #trackbars = cv2.createTrackbar("threshold", thr, 0)
 #resizeWindow(trackbarWindow, 640,480)
-cv2.createTrackbar("Threshold", trackbarWindow, 30, 100, on_trackbar)
+#cv2.createTrackbar("Threshold", trackbarWindow, 30, 100, on_trackbar)
+
+
+status = 0
+switch = 0
+
 
 while True:
+    
     key=cv2.waitKey(1)
     check, frame = video.read()
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
@@ -32,7 +47,7 @@ while True:
     
 
 
-    thr = int(cv2.getTrackbarPos("Threshold",trackbarWindow))
+    #thr = int(cv2.getTrackbarPos("Threshold",trackbarWindow))
 
     delta_frame = cv2.absdiff(gray, first_frame)
     delta_threshold = cv2.threshold(delta_frame,thr,255,cv2.THRESH_BINARY)[1]
@@ -41,22 +56,52 @@ while True:
     (cnts,_) = cv2.findContours(delta_threshold.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)    
 
     for contour in cnts:
-        if cv2.contourArea(contour) < 1000:
-            continue
-        status = 1
+        if cv2.contourArea(contour) < 10000:  
+            continue    
         (x,y,w,h) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x,y), (x+h, y+h), (0,255,0), 3)
+        status = 1      
+         
         
+         
+   
+
+  
+
+    if status == 1 and switch == 0:
+        switch = 1
+        print ("start")
+        times.append(datetime.now())
+    if status == 0 and switch == 1:
+        switch = 0
+        print ("end")
+        times.append(datetime.now())
+
+
+    status = 0
+
     cv2.imshow("video", gray)
     cv2.imshow("delta", delta_frame)
     cv2.imshow("normal", frame)
     cv2.imshow("Thresh", delta_threshold)
     if key==ord('q'):
+        if status == 1:
+            times.append(datetime.now())
         break
 
+end = None
+if len(times) % 2 != 0:
+    end = len(times) -1
+else:
+    end = len(times)
+for i in range(0, end,2):
+    df = df.append({"Start":times[i], "End":times[i+1]}, ignore_index=True)
 
+df.to_csv("Times.csv")
     
-    
+
+
+
     
 video.release()
 
